42f5247bd0bf7c67405418984cac3d1a91cb9a68
sahil sharma <sahilkumar4544.com>
2021-03-24 17:56:05 +0530

    adding template literals

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const fname = "Sahil Sharma";
+const fname = "Sahil";
+const lname = "Sharma";
 
 ReactDOM.render(
   <>
-   <h1>My name is {fname}</h1>
+   <h1>{`My name is ${fname} ${lname}`}</h1>
    <p>my lucky number is {Math.random()}</p>
   </>,
 document.getElementById("root")
